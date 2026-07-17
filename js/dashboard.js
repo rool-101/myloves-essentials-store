353c7ec9f14894b86e7204d4ecc01d477c16e7bd
Nano noubke <169007121+rool-101@users.noreply.github.com>
2026-07-17 23:35:19 +0200

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,37 +1,44 @@
 const SUPABASE_URL = "https://akuiyfasztszalelihvi.supabase.co";
 const SUPABASE_KEY = "sb_publishable_LNCF4fhHCcJUNW1y_vuoIg_eBBLkkPv";
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
 
-document.getElementById("saveProduct").addEventListener("click", async () => {
+const saveButton = document.getElementById("saveProduct");
+const status = document.getElementById("status");
+
+saveButton.addEventListener("click", async () => {
 
     const name = document.getElementById("name").value;
     const price = document.getElementById("price").value;
     const category = document.getElementById("category").value;
     const description = document.getElementById("description").value;
 
+    status.textContent = "Saving product...";
+
     const { error } = await supabase
         .from("products")
         .insert([
             {
-                name,
-                price,
-                category,
-                description
+                name: name,
+                price: price,
+                category: category,
+                description: description
             }
         ]);
 
-    const status = document.getElementById("status");
-
     if (error) {
-        status.textContent = "❌ " + error.message;
-    } else {
-        status.textContent = "✅ Product added successfully!";
-
-        document.getElementById("name").value = "";
-        document.getElementById("price").value = "";
-        document.getElementById("category").value = "";
-        document.getElementById("description").value = "";
+        status.textContent = error.message;
+        return;
     }
+
+    status.textContent = "Product saved successfully!";
+
+    document.getElementById("name").value = "";
+    document.getElementById("price").value = "";
+    document.getElementById("category").value = "";
+    document.getElementById("description").value = "";
 
 });
